cbdae636e0f0c5b3e6e46eed42898246710ecb98
Willdorf <william.g.armstrong@gmail.com>
2015-10-20 11:47:01 -0400

    project setup

--- src/circlesquared.c
+++ src/circlesquared.c
@@ -1,35 +1,71 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static Layer *s_layer;
+
+static uint8_t s_hour;
+static uint8_t s_min;
+static uint8_t s_sec;
+
+static int PADDING = 12;
+static int HEIGHT = 25;
+
+static void update_time(struct tm * tick_time) {
+	s_hour = tick_time->tm_hour;
+	s_min = tick_time->tm_min;
+	s_sec = tick_time->tm_sec;
+	layer_mark_dirty(s_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time(tick_time);
+}
+
+static void draw_watchface(Layer *layer, GContext *ctx) {
+	GRect bounds = layer_get_bounds(layer);
+	uint16_t width = bounds.size.w - (2 * PADDING);
+}
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
+
+	s_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
+	layer_add_child(window_get_root_layer(window), s_layer);
+	layer_set_update_proc(s_layer, draw_watchface);
 }
 
 static void window_unload(Window *window) {
+
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+	window = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+			.load = window_load,
+			.unload = window_unload,
+			});
+	const bool animated = true;
+	window_stack_push(window, animated);
+
+	//Register with TickTimerService
+	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+
+	//display the time right away
+	time_t start_time = time(NULL);
+	update_time(localtime(&start_time));
 }
 
 static void deinit(void) {
-  window_destroy(window);
+	tick_timer_service_unsubscribe();
+	window_destroy(window);
 }
 
 int main(void) {
-  init();
+	init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+	app_event_loop();
+	deinit();
 }
