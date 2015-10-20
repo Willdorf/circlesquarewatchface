eaf35ed787d8df4cde5e5c0b2673c09e37969472
Willdorf <william.g.armstrong@gmail.com>
2015-10-20 12:28:52 -0400

    added seconds

--- src/circlesquared.c
+++ src/circlesquared.c
@@ -10,6 +10,9 @@ static uint8_t s_sec;
 static int PADDING = 12;
 static int HEIGHT = 25;
 
+static GBitmap *s_ternary_seconds_bitmap;
+static BitmapLayer *s_ternary_seconds_layer;
+
 static void update_time(struct tm * tick_time) {
 	s_hour = tick_time->tm_hour;
 	s_min = tick_time->tm_min;
@@ -24,6 +27,12 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void draw_watchface(Layer *layer, GContext *ctx) {
 	GRect bounds = layer_get_bounds(layer);
 	uint16_t width = bounds.size.w - (2 * PADDING);
+
+	//set the colour
+	graphics_context_set_fill_color(ctx, COLOR_FALLBACK(GColorRed, GColorWhite));	
+
+	//display the seconds
+	graphics_fill_rect(ctx, GRect(((s_sec * width)/60) + PADDING, 84 - 10, 2, 19), 0 ,0);
 }
 
 static void window_load(Window *window) {
@@ -33,9 +42,21 @@ static void window_load(Window *window) {
 	s_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
 	layer_add_child(window_get_root_layer(window), s_layer);
 	layer_set_update_proc(s_layer, draw_watchface);
+
+	uint16_t width = bounds.size.w - (2 * PADDING);
+
+	//create the ternary seconds image
+	s_ternary_seconds_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SECONDS_TERNARY_REFLECT);
+	s_ternary_seconds_layer = bitmap_layer_create(GRect(PADDING, 0, width + 1, 168));
+	bitmap_layer_set_background_color(s_ternary_seconds_layer, GColorClear);
+	bitmap_layer_set_bitmap(s_ternary_seconds_layer, s_ternary_seconds_bitmap);
+	bitmap_layer_set_compositing_mode(s_ternary_seconds_layer, GCompOpSet);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_ternary_seconds_layer));
 }
 
 static void window_unload(Window *window) {
+	gbitmap_destroy(s_ternary_seconds_bitmap);
+	bitmap_layer_destroy(s_ternary_seconds_layer);
 
 }
 
