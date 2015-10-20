02dd47d8053a7197ba4e6dc03642c6a0307b6031
Willdorf <william.g.armstrong@gmail.com>
2015-10-20 14:43:47 -0400

    added hours

--- src/circlesquared.c
+++ src/circlesquared.c
@@ -13,6 +13,10 @@ static int HEIGHT = 25;
 static GBitmap *s_ternary_seconds_bitmap;
 static BitmapLayer *s_ternary_seconds_layer;
 
+static GBitmap *s_unary_hours_bitmap;
+static RotBitmapLayer *s_unary_hours_layer_left;
+static RotBitmapLayer *s_unary_hours_layer_right;
+
 static void update_time(struct tm * tick_time) {
 	s_hour = tick_time->tm_hour;
 	s_min = tick_time->tm_min;
@@ -33,6 +37,44 @@ static void draw_watchface(Layer *layer, GContext *ctx) {
 
 	//display the seconds
 	graphics_fill_rect(ctx, GRect(((s_sec * width)/60) + PADDING, 84 - 10, 2, 19), 0 ,0);
+
+	//display the hours
+
+	//left hours------------------------------ 
+	int8_t cur_hours = s_hour % 12;
+	int8_t left_hours = cur_hours;
+	if (cur_hours > 6) {
+		left_hours = 6;
+	}
+	GPathInfo HOURS_LEFT_PATH_INFO = {
+		.num_points = 4,
+		.points = (GPoint []) {{0,0},{left_hours*width/12,0},{left_hours*width/12,27},{0,27}}
+	};
+
+	GPath *s_hours_path_left = gpath_create(&HOURS_LEFT_PATH_INFO);
+	gpath_rotate_to(s_hours_path_left, DEG_TO_TRIGANGLE(-45));
+	gpath_move_to(s_hours_path_left, GPoint(3,32 + PADDING));
+	gpath_draw_filled(ctx, s_hours_path_left);
+
+	//right hours------------------------------ 
+	
+	int8_t right_hours = cur_hours;
+	if (cur_hours < 6) {
+		right_hours = 0;
+	} else {
+		right_hours = cur_hours - 6;
+	}
+
+	GPathInfo HOURS_RIGHT_PATH_INFO = {
+		.num_points = 4,
+		.points = (GPoint []) {{0,0},{right_hours*width/12,0},{right_hours*width/12,27},{0,27}}
+	};
+
+	GPath *s_hours_path_right = gpath_create(&HOURS_RIGHT_PATH_INFO);
+	gpath_rotate_to(s_hours_path_right, DEG_TO_TRIGANGLE(45));
+	gpath_move_to(s_hours_path_right, GPoint(width - 32 + 8, 1));
+	gpath_draw_filled(ctx, s_hours_path_right);
+
 }
 
 static void window_load(Window *window) {
@@ -52,11 +94,40 @@ static void window_load(Window *window) {
 	bitmap_layer_set_bitmap(s_ternary_seconds_layer, s_ternary_seconds_bitmap);
 	bitmap_layer_set_compositing_mode(s_ternary_seconds_layer, GCompOpSet);
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_ternary_seconds_layer));
+
+	//create the left hours image
+	s_unary_hours_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_6_SINGLE_BLOCKS);
+	s_unary_hours_layer_left = rot_bitmap_layer_create(s_unary_hours_bitmap);
+	rot_bitmap_layer_set_corner_clip_color(s_unary_hours_layer_left, GColorClear);
+	rot_bitmap_set_compositing_mode(s_unary_hours_layer_left, GCompOpSet);
+	rot_bitmap_layer_set_angle(s_unary_hours_layer_left, DEG_TO_TRIGANGLE(-45));
+	//position the frame
+	GRect r = layer_get_frame((Layer *) s_unary_hours_layer_left);
+	r.origin.x = 2;
+	layer_set_frame((Layer *) s_unary_hours_layer_left, r);
+	layer_add_child(window_get_root_layer(window), (Layer *) s_unary_hours_layer_left);
+
+	//create the right hours image
+	s_unary_hours_layer_right = rot_bitmap_layer_create(s_unary_hours_bitmap);
+	rot_bitmap_layer_set_corner_clip_color(s_unary_hours_layer_right, GColorClear);
+	rot_bitmap_set_compositing_mode(s_unary_hours_layer_right, GCompOpSet);
+	rot_bitmap_layer_set_angle(s_unary_hours_layer_right, DEG_TO_TRIGANGLE(45));
+	//position the frame
+	r = layer_get_frame((Layer *) s_unary_hours_layer_right);
+	r.origin.x = width - 32 - PADDING; 
+	layer_set_frame((Layer *) s_unary_hours_layer_right, r);
+	layer_add_child(window_get_root_layer(window), (Layer *) s_unary_hours_layer_right);
+	
+
 }
 
 static void window_unload(Window *window) {
 	gbitmap_destroy(s_ternary_seconds_bitmap);
 	bitmap_layer_destroy(s_ternary_seconds_layer);
+
+	gbitmap_destroy(s_unary_hours_bitmap);
+	rot_bitmap_layer_destroy(s_unary_hours_layer_left);
+	rot_bitmap_layer_destroy(s_unary_hours_layer_right);
 
 }
 
